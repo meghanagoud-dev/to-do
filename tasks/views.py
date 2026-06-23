02a637bc2c8903a9ab20a258241efdd02f6a3ecb
Meghanagoud-dev <meghanagoud149@gmail.com>
2026-06-23 17:26:21 +0530

    Fix delete_task TypeError-param name mismatch

--- tasks/views.py
+++ tasks/views.py
@@ -12,8 +12,8 @@ def home(request):
     tasks = Task.objects.all().order_by('-id')
     return render(request, 'tasks/home.html', {'tasks': tasks})
 
-def delete_task(request, id):
-    task = Task.objects.get(id=id)
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('home')
 def complete_task(request, id):
