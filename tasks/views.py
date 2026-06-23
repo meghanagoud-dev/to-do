c284a2f374d0f68e48376bef294b661a3c8df77c
Meghana Goud <meghanagoud149@gmail.com>
2026-06-23 11:30:12 +0530

    Ready for deployment

---
+++ tasks/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import Task
+
+def home(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        due_date = request.POST.get('due_date')
+        if title:
+            Task.objects.create(title=title, due_date=due_date if due_date else None)
+        return redirect('home')
+
+    tasks = Task.objects.all().order_by('-id')
+    return render(request, 'tasks/home.html', {'tasks': tasks})
+
+def delete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.delete()
+    return redirect('home')
+def complete_task(request, id):
+    task = Task.objects.get(id=id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('home')
