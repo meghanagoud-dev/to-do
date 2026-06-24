3b319f541350bcf9910f4a1eb43d85c6667677a9
Meghana Goud <meghanagoud149@gmail.com>
2026-06-24 07:40:34 +0530

    Fix: Production settings for Render

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%(73f4%y=u##^ed#=@+8qy76i8)h5e^*)k)+9yhi9ogyf52t3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*''to-do-epfl.onrender.com']
 
 
 # Application definition
