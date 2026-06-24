32b29ab04ab547e18801d2d23cd40c6870ad3a03
Meghana Goud <meghanagoud149@gmail.com>
2026-06-24 10:44:36 +0530

    Add register template

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('complete/<int:id>/', views.complete_task, name='complete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 
 @login_required
@@ -13,4 +14,12 @@ def home(request):
     
     tasks = Task.objects.filter(user=request.user).order_by('-id')
     return render(request, 'home.html', {'tasks': tasks})
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
