40b2fdfbdb3736819b48d1b6b8f3a861b16e02b2
Meghana Goud <meghanagoud149@gmail.com>
2026-06-25 15:43:35 +0530

    Fix: Change logout link to POST form

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-from django.contrib.auth import logout
-from django.shortcuts import redirect
-def logout_view(request):
-    logout(request)
-    return redirect('login')
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('complete/<int:id>/', views.complete_task, name='complete_task'),
-        path('logout/', logout_view, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
