546b458cc5382f7c091ab00e7f86aaf412d3d2b8
Meghana Goud <meghanagoud149@gmail.com>
2026-06-25 18:11:34 +0530

    Plan B: Full nuke settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 # --- PLAN B: NUKE DB CODE START ---
 import sqlite3
