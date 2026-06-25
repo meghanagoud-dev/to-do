ee12258dfaeb3cf816d30f98c76c7a28fbcaa423
Meghana Goud <meghanagoud149@gmail.com>
2026-06-25 09:14:14 +0530

    turn on debug to see error

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%(73f4%y=u##^ed#=@+8qy76i8)h5e^*)k)+9yhi9ogyf52t3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','to-do-epff.onrender.com']
 
