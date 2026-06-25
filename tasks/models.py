5f84b82e4c5a4607a01dbc11076d50695e7783b8
Meghana Goud <meghanagoud149@gmail.com>
2026-06-25 15:17:34 +0530

    Final fix: Add user field and due_date to Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    due_date = models.DateField(null=True, blank=True)  # <- IDI IMPORTANT
+    due_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.title
