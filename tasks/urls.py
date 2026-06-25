194fe962471d5fdfa148ad633ce379d8ea6184d2
Meghana Goud <meghanagoud149@gmail.com>
2026-06-25 10:56:31 +0530

    added logout fix for 405 error

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+def logout_view(request):
+    logout(request)
+    return redirect('login')
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
     path('complete/<int:id>/', views.complete_task, name='complete_task'),
+        path('logout/', logout_view, name='logout'),
 ]
