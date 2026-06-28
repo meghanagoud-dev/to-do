350312c776a4cdbe007c5462ca564df0e59cb6e7
Meghanagoud-dev <meghanagoud149@gmail.com>
2026-06-28 11:28:41 +0530

    Update superuser creation in build script
    Replaced superuser creation command with createsuperuser.

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Auto create superuser 'mawa' if not exists
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='mawa').exists() or User.objects.create_superuser('mawa', '', 'Mawa@1234')"
+python manage.py createsuperuser --noinput || true
